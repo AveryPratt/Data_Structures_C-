f01fe02aca45c1cf95360794ca9f3ca0ab6f864c
Avery Pratt <apratt91@gmail.com>
2017-11-26 14:41:19 -0800

    added a stack composed of a linked list

---
+++ ConsoleApp1/ConsoleApp1/Stack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Stack<T>
+    {
+        public LinkedList<T> List { get; private set; }
+
+        public void Push(T val)
+        {
+            this.List.Push(val);
+        }
+
+        public T Pop()
+        {
+            return this.List.Pop();
+        }
+    }
+}
