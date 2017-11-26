48904e4cf2e3b45aa778931b7f98838007610e2d
Avery Pratt <apratt91@gmail.com>
2017-11-26 14:50:35 -0800

    added queue composed of doubly-linked list

--- ConsoleApp1/ConsoleApp1/DoublyLinkedList.cs
+++ ConsoleApp1/ConsoleApp1/DoublyLinkedList.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace DataStructures
 {
     public class DoublyLinkedListNode<T>
     {
--- ConsoleApp1/ConsoleApp1/Queue.cs
+++ ConsoleApp1/ConsoleApp1/Queue.cs
@@ -5,7 +5,35 @@ using System.Threading.Tasks;
 
 namespace DataStructures
 {
-    class Queue
+    class Queue<T>
     {
+        public DoublyLinkedList<T> List { get; set; }
+
+        public void Enqueue(T val)
+        {
+            this.List.Append(val);
+        }
+        
+        public T Dequeue()
+        {
+            return this.List.Shift().Data;
+        }
+
+        public int Size()
+        {
+            int count = 0;
+            DoublyLinkedListNode<T> cur = List.Head;
+            while (cur != null)
+            {
+                cur = cur.Next;
+                count++;
+            }
+            return count;
+        }
+
+        public T Peek()
+        {
+            return List.Tail.Data;
+        }
     }
 }
