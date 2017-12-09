88f7d8dee90007f30b401f0729f4f88ed266ae6b
Avery Pratt <avery.pratt@allyis.com>
2017-12-08 17:06:00 -0800

    added tests for trie

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,18 +9,6 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-			PriorityQueue<string> queue = new PriorityQueue<string>();
-			Random rand = new Random();
-			for (int i = 0; i < 1000; i++)
-			{
-				var val = rand.Next();
-				queue.Insert(val.ToString(), val);
-			}
-			while (queue.Heap.Values.Count > 0)
-			{
-				Console.WriteLine(queue.Pop());
-			}
-			Console.ReadLine();
 		}
     }
 }
